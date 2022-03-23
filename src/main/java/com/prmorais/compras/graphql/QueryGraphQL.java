e2cd0849e1def6d8123e4b65811431c8a4f478c7
PauloDev13 <devpgm2020@gmail.com>
2022-03-23 12:28:46 -0300

    Creating custom type Customer

--- src/main/java/com/prmorais/compras/graphql/QueryGraphQL.java
+++ src/main/java/com/prmorais/compras/graphql/QueryGraphQL.java
@@ -1,6 +1,7 @@
 package com.prmorais.compras.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.prmorais.compras.types.Client;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +12,9 @@ public class QueryGraphQL implements GraphQLQueryResolver {
 
   public int soma(int a, int b) {
     return a + b;
+  }
+
+  public Client getClient() {
+    return new Client("Paulo", "prmorais1302@gmail.com");
   }
 }
---
+++ src/main/java/com/prmorais/compras/types/Client.java
@@ -0,0 +1,14 @@
+package com.prmorais.compras.types;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+  private String name;
+  private String email;
+}
