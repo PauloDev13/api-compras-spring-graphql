f551cfea9e58545e4671960c03fff119dcfdd216
PauloDev13 <devpgm2020@gmail.com>
2022-03-31 19:19:20 -0300

    Implementing JPA caching

--- src/main/java/com/prmorais/compras/repositories/ProductRepository.java
+++ src/main/java/com/prmorais/compras/repositories/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.prmorais.compras.repositories;
 
 import com.prmorais.compras.models.Product;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.QueryHint;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  @NotNull
+  @Override
+  @QueryHints({ @QueryHint(name = "org.hibernate.cacheable", value = "true") })
+  List<Product> findAll();
 }
--- src/main/java/com/prmorais/compras/repositories/PurchaseRepository.java
+++ src/main/java/com/prmorais/compras/repositories/PurchaseRepository.java
@@ -2,12 +2,24 @@ package com.prmorais.compras.repositories;
 
 import com.prmorais.compras.models.Client;
 import com.prmorais.compras.models.Purchase;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.QueryHint;
 import java.util.List;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+
+//  @NotNull
+//  @Override
+//  @QueryHints({ @QueryHint(name = "org.hibernate.cacheable", value = "true") })
+//  Page<Purchase> findAll(@NotNull Pageable pageable);
+
+  @QueryHints({ @QueryHint(name = "org.hibernate.cacheable", value = "true") })
   List<Purchase> findAllByClient(Client client);
 }
